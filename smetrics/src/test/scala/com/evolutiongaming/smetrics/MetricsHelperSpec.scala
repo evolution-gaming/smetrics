0c5e0ab53a46b69c42bc1c8c74212603bded7190
Yaroslav Klymko <t3hnar@gmail.com>
2019-06-30 19:40:33 +0200

    improve MetricsHelper

---
+++ smetrics/src/test/scala/com/evolutiongaming/smetrics/MetricsHelperSpec.scala
@@ -0,0 +1,15 @@
+package com.evolutiongaming.smetrics
+
+import com.evolutiongaming.smetrics.MetricsHelper._
+import org.scalatest.{FunSuite, Matchers}
+
+class MetricsHelperSpec extends FunSuite with Matchers {
+
+  test("nanosToSeconds") {
+    1000000.nanosToSeconds shouldEqual 1
+  }
+
+  test("millisToSeconds") {
+    1000.millisToSeconds shouldEqual 1
+  }
+}
