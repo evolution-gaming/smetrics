3f7d36ef0811eebf925c4df6dd7a38ad5c89026a
Scala Steward <me@scala-steward.org>
2020-10-16 08:38:58 +0200

    Update http4s-core to 0.21.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val prometheusCommon = "io.prometheus"        % "simpleclient_common" % prometheusVersion
   val scalatest        = "org.scalatest"       %% "scalatest"           % "3.2.1"
   val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"         % "2.0.4"
-  val http4s           = "org.http4s"          %% "http4s-core"         % "0.21.6"
+  val http4s           = "org.http4s"          %% "http4s-core"         % "0.21.8"
   val doobie           = "org.tpolecat"        %% "doobie-core"         % "0.9.0"
 
   object Cats {
