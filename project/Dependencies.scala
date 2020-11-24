366f33215742f2e9cef6fd3914e0f7bdcb9c5328
Scala Steward <me@scala-steward.org>
2020-11-24 22:39:37 +0100

    Update doobie-core to 0.9.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val scalatest        = "org.scalatest"       %% "scalatest"           % "3.2.3"
   val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"         % "2.1.3"
   val http4s           = "org.http4s"          %% "http4s-core"         % "0.21.6"
-  val doobie           = "org.tpolecat"        %% "doobie-core"         % "0.9.0"
+  val doobie           = "org.tpolecat"        %% "doobie-core"         % "0.9.4"
 
   object Cats {
     private val version = "2.1.1"
