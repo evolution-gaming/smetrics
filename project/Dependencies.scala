3944c315ddd3689193289e6b2f9aeea2d0699f48
Scala Steward <me@scala-steward.org>
2021-11-30 20:31:53 +0100

    Update scalatest to 3.2.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   private val prometheusVersion = "0.9.0"
   val prometheus       = "io.prometheus"        % "simpleclient"        % prometheusVersion
   val prometheusCommon = "io.prometheus"        % "simpleclient_common" % prometheusVersion
-  val scalatest        = "org.scalatest"       %% "scalatest"           % "3.2.9"
+  val scalatest        = "org.scalatest"       %% "scalatest"           % "3.2.10"
   val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"         % "2.6.2"
   val http4s           = "org.http4s"          %% "http4s-core"         % "0.22.7"
   val doobie           = "org.tpolecat"        %% "doobie-core"         % "0.9.0"
