ec270d1a360b64a2dc04c1741214c8eb984f4257
Scala Steward <me@scala-steward.org>
2021-11-30 20:31:19 +0100

    Update cats-helper to 2.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val prometheus       = "io.prometheus"        % "simpleclient"        % prometheusVersion
   val prometheusCommon = "io.prometheus"        % "simpleclient_common" % prometheusVersion
   val scalatest        = "org.scalatest"       %% "scalatest"           % "3.2.9"
-  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"         % "2.6.2"
+  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"         % "2.7.0"
   val http4s           = "org.http4s"          %% "http4s-core"         % "0.22.7"
   val doobie           = "org.tpolecat"        %% "doobie-core"         % "0.9.0"
 
