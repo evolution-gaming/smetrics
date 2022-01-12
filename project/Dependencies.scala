c839be6bda55cde4ff72743bd7620c1fcb752170
Andrei Mikhailov <amikhailov@evolution.com>
2022-01-12 17:20:03 +0300

    Cats-helper version to 3.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,7 @@ object Dependencies {
   val prometheus       = "io.prometheus"        % "simpleclient"        % prometheusVersion
   val prometheusCommon = "io.prometheus"        % "simpleclient_common" % prometheusVersion
   val scalatest        = "org.scalatest"       %% "scalatest"           % "3.2.9"
-  // TODO replace with real version after migration https://github.com/evolution-gaming/cats-helper/pull/103
-  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"         % "2.6.3-SNAPSHOT"
+  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"         % "3.0.0"
   val http4s           = "org.http4s"          %% "http4s-core"         % "0.23.7"
   val doobie           = "org.tpolecat"        %% "doobie-core"         % "1.0.0-RC1"
 
