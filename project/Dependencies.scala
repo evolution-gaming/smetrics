624980e9801693bf85445dcfe4b10e796600bbb9
Scala Steward <me@scala-steward.org>
2022-02-06 05:03:20 +0100

    Update simpleclient_common to 0.15.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  private val prometheusVersion = "0.9.0"
+  private val prometheusVersion = "0.15.0"
   val prometheus       = "io.prometheus"        % "simpleclient"        % prometheusVersion
   val prometheusCommon = "io.prometheus"        % "simpleclient_common" % prometheusVersion
   val scalatest        = "org.scalatest"       %% "scalatest"           % "3.2.9"
