88087c222ce09ad7944e7d42d66d71176c9c138e
Yaroslav Klymko <t3hnar@gmail.com>
2022-12-09 18:38:18 +0100

    update and cleanup dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,15 +6,13 @@ object Dependencies {
   val prometheus       = "io.prometheus"        % "simpleclient"        % prometheusVersion
   val prometheusCommon = "io.prometheus"        % "simpleclient_common" % prometheusVersion
   val scalatest        = "org.scalatest"       %% "scalatest"           % "3.2.13"
-  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"         % "3.1.1"
+  val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"         % "3.3.0"
   val http4s           = "org.http4s"          %% "http4s-core"         % "0.23.7"
   val doobie           = "org.tpolecat"        %% "doobie-core"         % "1.0.0-RC2"
   val skafka           = "com.evolutiongaming" %% "skafka"              % "14.1.2"
 
   object Cats {
-    private val version = "2.7.0"
-    private val `effects-version` = "3.3.4"
-    val core   = "org.typelevel" %% "cats-core"   % version
-    val effect = "org.typelevel" %% "cats-effect" % `effects-version`
+    val core   = "org.typelevel" %% "cats-core"   % "2.9.0"
+    val effect = "org.typelevel" %% "cats-effect" % "3.3.14"
   }
 }
