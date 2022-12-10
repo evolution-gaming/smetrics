13fb1e969e48011f6889f2b4d7015b985e15b904
Scala Steward <scala_steward@virtuslab.com>
2022-12-10 15:24:53 +0000

    Revert commit(s) 5792e0e

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   private val prometheusVersion = "0.9.0"
   val prometheus       = "io.prometheus"        % "simpleclient"        % prometheusVersion
   val prometheusCommon = "io.prometheus"        % "simpleclient_common" % prometheusVersion
-  val scalatest        = "org.scalatest"       %% "scalatest"           % "3.2.14"
+  val scalatest        = "org.scalatest"       %% "scalatest"           % "3.2.13"
   val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"         % "3.1.1"
   val http4s           = "org.http4s"          %% "http4s-core"         % "0.23.7"
   val doobie           = "org.tpolecat"        %% "doobie-core"         % "1.0.0-RC2"
