694b3c232a6876dcd9bec737311306cc81ab9ba9
Denys Fakhritdinov <dfakhritdinov@evolution.com>
2023-07-10 15:17:16 +0200

    bump skafka to 15.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,11 +5,11 @@ object Dependencies {
   private val prometheusVersion = "0.9.0"
   val prometheus       = "io.prometheus"        % "simpleclient"        % prometheusVersion
   val prometheusCommon = "io.prometheus"        % "simpleclient_common" % prometheusVersion
-  val scalatest        = "org.scalatest"       %% "scalatest"           % "3.2.13"
+  val scalatest        = "org.scalatest"       %% "scalatest"           % "3.2.15"
   val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"         % "3.5.0"
-  val http4s           = "org.http4s"          %% "http4s-core"         % "0.23.7"
+  val http4s           = "org.http4s"          %% "http4s-core"         % "0.23.18"
   val doobie           = "org.tpolecat"        %% "doobie-core"         % "1.0.0-RC2"
-  val skafka           = "com.evolutiongaming" %% "skafka"              % "14.1.2"
+  val skafka           = "com.evolutiongaming" %% "skafka"              % "15.2.0"
 
   object Cats {
     val core   = "org.typelevel" %% "cats-core"   % "2.9.0"
