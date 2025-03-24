05986cbd84229bad672b2cdc557e72d372579569
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-03-24 19:23:11 +0100

    Update doobie-core to 1.0.0-RC8 (#324)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val scalatest        = "org.scalatest"       %% "scalatest"           % "3.2.15"
   val `cats-helper`    = "com.evolutiongaming" %% "cats-helper"         % "3.7.0"
   val http4s           = "org.http4s"          %% "http4s-core"         % "0.23.7"
-  val doobie           = "org.tpolecat"        %% "doobie-core"         % "1.0.0-RC6"
+  val doobie           = "org.tpolecat"        %% "doobie-core"         % "1.0.0-RC8"
 
   object Cats {
     val core   = "org.typelevel" %% "cats-core"   % "2.9.0"
