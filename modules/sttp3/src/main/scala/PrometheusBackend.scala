544db3a1ab7420f070bf0b7651f146b6c654d72c
Oleg Kovalenko <okovalenko@evolution.com>
2025-11-12 15:16:48 +0100

    define PrometheusBackend.apply

--- modules/sttp3/src/main/scala/PrometheusBackend.scala
+++ modules/sttp3/src/main/scala/PrometheusBackend.scala
@@ -7,9 +7,9 @@ import cats.effect.Resource
 // import com.evolutiongaming.smetrics._
 
 // import java.util.concurrent.ConcurrentHashMap
-// import sttp.client3.{FollowRedirectsBackend, HttpError, Identity, Request, Response, SttpBackend}
+import sttp.client3._
 // import io.prometheus.client.{CollectorRegistry, Counter, Gauge, Histogram, Summary}
-// import sttp.client3.listener.{ListenerBackend, RequestListener}
+import sttp.client3.listener._
 // import sttp.client3.prometheus.PrometheusBackend.RequestCollectors
 // import sttp.model.StatusCode
 import com.evolutiongaming.smetrics._
@@ -19,10 +19,170 @@ import com.evolutiongaming.smetrics._
 
 object PrometheusBackend {
 
-  def of[F[_]: Monad](
-      collectorRegistry: CollectorRegistry[F],
-  ): Resource[F, Unit] = {
-    ???
+  def apply[F[_], P](
+      delegate: SttpBackend[F, P],
+      requestToHistogramNameMapper: Request[_, _] => Option[Histogram[F]],
+      requestToInProgressGaugeNameMapper: Request[_, _] => Option[Gauge[F]],
+      responseToSuccessCounterMapper: (Request[_, _], Response[_]) => Option[Counter[F]],
+      responseToErrorCounterMapper: (Request[_, _], Response[_]) => Option[Counter[F]],
+      requestToFailureCounterMapper: (Request[_, _], Throwable) => Option[Counter[F]],
+      requestToSizeSummaryMapper: Request[_, _] => Option[Summary[F]],
+      responseToSizeSummaryMapper: (Request[_, _], Response[_]) => Option[Summary[F]],
+  ): SttpBackend[F, P] = {
+    // redirects should be handled before prometheus
+    new FollowRedirectsBackend[F, P](
+      new ListenerBackend[F, P, RequestCollectors](
+        delegate,
+        new PrometheusListener[F](
+          (req: Request[_, _]) => requestToHistogramNameMapper(req),
+          (req: Request[_, _]) => requestToInProgressGaugeNameMapper(req),
+          (rr: (Request[_, _], Response[_])) => responseToSuccessCounterMapper(rr._1, rr._2),
+          (rr: (Request[_, _], Response[_])) => responseToErrorCounterMapper(rr._1, rr._2),
+          (r: (Request[_, _], Throwable)) => requestToFailureCounterMapper(r._1, r._2),
+          (req: Request[_, _]) => requestToSizeSummaryMapper(req),
+          (rr: (Request[_, _], Response[_])) => responseToSizeSummaryMapper(rr._1, rr._2),
+        ),
+      )
+    )
+  }
+
+  private[PrometheusBackend] final case class RequestCollectors()
+
+  class PrometheusListener[F[_]](
+      requestToHistogramNameMapper: Request[_, _] => Option[HistogramCollectorConfig],
+      requestToInProgressGaugeNameMapper: Request[_, _] => Option[CollectorConfig],
+      requestToSuccessCounterMapper: ((Request[_, _], Response[_])) => Option[CollectorConfig],
+      requestToErrorCounterMapper: ((Request[_, _], Response[_])) => Option[CollectorConfig],
+      requestToFailureCounterMapper: ((Request[_, _], Exception)) => Option[CollectorConfig],
+      requestToSizeSummaryMapper: Request[_, _] => Option[CollectorConfig],
+      responseToSizeSummaryMapper: ((Request[_, _], Response[_])) => Option[CollectorConfig],
+  ) extends RequestListener[F, RequestCollectors] {
+
+    //
+    //  override def beforeRequest(request: Request[_, _]): F[RequestCollectors] = {
+    //    val requestTimer: Option[Histogram.Timer] = for {
+    //      histogramData       <- requestToHistogramNameMapper(request)
+    //      histogram: Histogram = getOrCreateMetric(histogramsCache, histogramData, createNewHistogram)
+    //    } yield histogram.labels(histogramData.labelValues: _*).startTimer()
+    //
+    //    val gauge: Option[Gauge.Child] = for {
+    //      gaugeData <- requestToInProgressGaugeNameMapper(request)
+    //    } yield getOrCreateMetric(gaugesCache, gaugeData, createNewGauge).labels(gaugeData.labelValues: _*)
+    //
+    //    observeRequestContentLengthSummaryIfMapped(request, requestToSizeSummaryMapper)
+    //
+    //    gauge.foreach(_.inc())
+    //
+    //    RequestCollectors(requestTimer, gauge)
+    //  }
+    //
+    //  override def requestException(
+    //      request: Request[_, _],
+    //      requestCollectors: RequestCollectors,
+    //      e: Exception
+    //  ): F[Unit] = {
+    //    HttpError.find(e) match {
+    //      case Some(HttpError(body, statusCode)) =>
+    //        requestSuccessful(request, Response(body, statusCode).copy(request = request.onlyMetadata), requestCollectors)
+    //      case _                                 =>
+    //        requestCollectors.maybeTimer.foreach(_.observeDuration())
+    //        requestCollectors.maybeGauge.foreach(_.dec())
+    //        incCounterIfMapped((request, e), requestToFailureCounterMapper)
+    //    }
+    //  }
+    //
+    //  override def requestSuccessful(
+    //      request: Request[_, _],
+    //      response: Response[_],
+    //      requestCollectors: RequestCollectors
+    //  ): F[Unit] = {
+    //    requestCollectors.maybeTimer.foreach(_.observeDuration())
+    //    requestCollectors.maybeGauge.foreach(_.dec())
+    //    observeResponseContentLengthSummaryIfMapped(request, response, responseToSizeSummaryMapper)
+    //
+    //    if (response.isSuccess) {
+    //      incCounterIfMapped((request, response), requestToSuccessCounterMapper)
+    //    } else {
+    //      incCounterIfMapped((request, response), requestToErrorCounterMapper)
+    //    }
+    //  }
+    //
+    //  private def incCounterIfMapped[T](
+    //      request: T,
+    //      mapper: T => Option[BaseCollectorConfig]
+    //  ): Unit =
+    //    mapper(request).foreach { data =>
+    //      getOrCreateMetric(countersCache, data, createNewCounter).labels(data.labelValues: _*).inc()
+    //    }
+    //
+    //  private def observeResponseContentLengthSummaryIfMapped(
+    //      request: Request[_, _],
+    //      response: Response[_],
+    //      mapper: ((Request[_, _], Response[_])) => Option[BaseCollectorConfig]
+    //  ): Unit =
+    //    mapper((request, response)).foreach { data =>
+    //      response.contentLength.map(_.toDouble).foreach { size =>
+    //        getOrCreateMetric(summariesCache, data, createNewSummary).labels(data.labelValues: _*).observe(size)
+    //      }
+    //    }
+    //
+    //  private def observeRequestContentLengthSummaryIfMapped(
+    //      request: Request[_, _],
+    //      mapper: Request[_, _] => Option[BaseCollectorConfig]
+    //  ): Unit =
+    //    mapper(request).foreach { data =>
+    //      (request.contentLength: Option[Long]).map(_.toDouble).foreach { size =>
+    //        getOrCreateMetric(summariesCache, data, createNewSummary).labels(data.labelValues: _*).observe(size)
+    //      }
+    //    }
+    //
+    //  private def getOrCreateMetric[T, C <: BaseCollectorConfig](
+    //      cache: ConcurrentHashMap[String, T],
+    //      data: C,
+    //      create: C => T
+    //  ): T =
+    //    cache.computeIfAbsent(
+    //      data.collectorName,
+    //      new java.util.function.Function[String, T] {
+    //        override def apply(t: String): T = create(data)
+    //      }
+    //    )
+    //
+    //  private def createNewHistogram(data: HistogramCollectorConfig): Histogram = {
+    //    Histogram
+    //      .build()
+    //      .buckets(data.buckets: _*)
+    //      .name(data.collectorName)
+    //      .labelNames(data.labelNames: _*)
+    //      .help(data.collectorName)
+    //      .register(collectorRegistry)
+    //
+    //    collectorRegistry.histogram(data.collectorName, data.collectorName, data.labelNames: _*)
+    //  }
+    //
+    //  private def createNewGauge(data: BaseCollectorConfig): Gauge =
+    //    Gauge
+    //      .build()
+    //      .name(data.collectorName)
+    //      .labelNames(data.labelNames: _*)
+    //      .help(data.collectorName)
+    //      .register(collectorRegistry)
+    //
+    //  private def createNewCounter(data: BaseCollectorConfig): Counter =
+    //    Counter
+    //      .build()
+    //      .name(data.collectorName)
+    //      .labelNames(data.labelNames: _*)
+    //      .help(data.collectorName)
+    //      .register(collectorRegistry)
+    //
+    //  private def createNewSummary(data: BaseCollectorConfig): Summary =
+    //    Summary
+    //      .build()
+    //      .name(data.collectorName)
+    //      .labelNames(data.labelNames: _*)
+    //      .help(data.collectorName)
+    //      .register(collectorRegistry)
   }
 }
 
