458c35008d7eda5661c709b248d70baff532c645
Oleg Kovalenko <okovalenko@evolution.com>
2025-11-13 15:52:44 +0100

    draft test

---
+++ modules/sttp3/src/test/scala/sttp/client3/smetrics/SmetricsBackendSpec.scala
@@ -0,0 +1,12 @@
+package sttp.client3.smetrics
+
+import cats._
+import cats.syntax.all._
+import cats.effect._
+import cats.effect.syntax.all._
+import org.scalatest.funsuite.AnyFunSuiteLike
+import org.scalatest.matchers.should.Matchers
+
+class SmetricsBackendSpec extends AnyFunSuiteLike with Matchers {
+
+}
