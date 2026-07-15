d23fce63618a1e7cf406e92bd2857d3aafd90498
Oleg Kovalenko <okovalenko@evolution.com>
2026-07-15 14:52:38 +0200

    bulk libs update (#434)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,24 +5,24 @@ object Dependencies {
   private val prometheusVersion = "0.9.0"
   val prometheus = "io.prometheus" % "simpleclient" % prometheusVersion
   val prometheusCommon = "io.prometheus" % "simpleclient_common" % prometheusVersion
-  val scalatest = "org.scalatest" %% "scalatest" % "3.2.19"
+  val scalatest = "org.scalatest" %% "scalatest" % "3.2.20"
   val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "3.12.2"
-  val http4s = "org.http4s" %% "http4s-core" % "0.23.33"
+  val http4s = "org.http4s" %% "http4s-core" % "0.23.36"
   val doobie = "org.tpolecat" %% "doobie-core" % "1.0.0-RC11"
 
   object Cats {
     val core = "org.typelevel" %% "cats-core" % "2.13.0"
-    val effect = "org.typelevel" %% "cats-effect" % "3.6.3"
+    val effect = "org.typelevel" %% "cats-effect" % "3.7.0"
   }
 
   object PrometheusV1 {
-    private val version = "1.4.3"
+    private val version = "1.8.0"
     val core = "io.prometheus" % "prometheus-metrics-core" % version
     val formats = "io.prometheus" % "prometheus-metrics-exposition-textformats" % version
   }
 
   object Logback {
-    private val version = "1.5.22"
+    private val version = "1.5.38"
     val classic = "ch.qos.logback" % "logback-classic" % version
   }
 
@@ -33,7 +33,7 @@ object Dependencies {
   }
 
   object Sttp4 {
-    private val version = "4.0.13"
+    private val version = "4.0.26"
     val core = "com.softwaremill.sttp.client4" %% "core" % version
     val catsBackend = "com.softwaremill.sttp.client4" %% "cats" % version
   }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,10 +4,10 @@ addSbtPlugin("org.scoverage" % "sbt-coveralls" % "1.3.15")
 
 addSbtPlugin("com.github.sbt" % "sbt-dynver" % "5.1.1")
 
-addSbtPlugin("ch.epfl.scala" % "sbt-version-policy" % "3.2.1")
+addSbtPlugin("ch.epfl.scala" % "sbt-version-policy" % "3.3.0")
 
 addSbtPlugin("com.evolution" % "sbt-scalac-opts-plugin" % "0.0.9")
 
 addSbtPlugin("com.evolution" % "sbt-artifactory-plugin" % "0.0.2")
 
-addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.5.6")
+addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.6.1")
